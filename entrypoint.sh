9907c1bce237f46dcbfea1dcb9400ebae3cb10ad
KyleUltimate <68589851+KyleUltimate@users.noreply.github.com>
2022-07-16 13:40:29 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 sudo sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
-mkdir build
-cd build
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si --noconfirm
 cd ../
 rm -rf paru
-cp /home/builder/.cache/paru/packages.aur /home/builder
+paru -Pc| grep AUR| sed 's/ .*//' > packages.aur
+mkdir build
+cd build
 mkdir pacarcarx;string=$(wc -l ../packages.aur); counts=${string%% *};for (( i=1; i<=$counts; i++ )); do echo $i;name=$(awk "NR==$i {print; exit}" ../packages.aur);echo $name;git clone "https://aur.archlinux.org/$name.git" ; cd $name; paru -U --noconfirm;cp $name*.pkg* ../pacarcarx ; cd ../ ; rm -rf $name;paru --clean --noconfirm; done
 
