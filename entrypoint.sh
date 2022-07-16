f7da2868b75831d50e8b91564d8fe000a87b801e
KyleUltimate <68589851+KyleUltimate@users.noreply.github.com>
2022-07-16 13:20:08 +0800

    wrarxcrrw

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,6 @@ cd paru
 makepkg -si --noconfirm
 cd ../
 rm -rf paru
-
+cp /home/builder/.cache/paru/packages.aur /home/builder
 mkdir pacarcarx;string=$(wc -l ../packages.aur); counts=${string%% *};for (( i=1; i<=$counts; i++ )); do echo $i;name=$(awk "NR==$i {print; exit}" ../packages.aur);echo $name;git clone "https://aur.archlinux.org/$name.git" ; cd $name; paru -U --noconfirm;cp $name*.pkg* ../pacarcarx ; cd ../ ; rm -rf $name;paru --clean --noconfirm; done
 
