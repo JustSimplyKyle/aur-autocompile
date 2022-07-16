c7212ab17062291478ebc238daea55f6a868deb8
KyleUltimate <68589851+KyleUltimate@users.noreply.github.com>
2022-07-16 13:07:15 +0800

    fucking save disk space

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 mkdir build
 cd build
-mkdir pacarcarx;string=$(wc -l ../packages.aur); counts=${string%% *};for (( i=1; i<=$counts; i++ )); do echo $i;name=$(awk "NR==$i {print; exit}" ../packages.aur);echo $name;git clone "https://aur.archlinux.org/$name.git" ; cd $name; makepkg -s --noconfirm;cp $name*.pkg* ../pacarcarx ; cd ../ ; rm -rf $name; done
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
+cd ../
+rm -rf paru
+mkdir pacarcarx;string=$(wc -l ../packages.aur); counts=${string%% *};for (( i=1; i<=$counts; i++ )); do echo $i;name=$(awk "NR==$i {print; exit}" ../packages.aur);echo $name;git clone "https://aur.archlinux.org/$name.git" ; cd $name; paru -U --noconfirm;cp $name*.pkg* ../pacarcarx ; cd ../ ; rm -rf $name;paru --clean; done
 
