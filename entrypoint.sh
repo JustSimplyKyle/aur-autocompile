9500b661eb6ba6d99b3a3ba1050e1a74fea8cca5
KyleUltimate <68589851+KyleUltimate@users.noreply.github.com>
2022-07-16 13:08:32 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ mkdir build
 cd build
 git clone https://aur.archlinux.org/paru.git
 cd paru
-makepkg -si
+makepkg -si --noconfirm
 cd ../
 rm -rf paru
 mkdir pacarcarx;string=$(wc -l ../packages.aur); counts=${string%% *};for (( i=1; i<=$counts; i++ )); do echo $i;name=$(awk "NR==$i {print; exit}" ../packages.aur);echo $name;git clone "https://aur.archlinux.org/$name.git" ; cd $name; paru -U --noconfirm;cp $name*.pkg* ../pacarcarx ; cd ../ ; rm -rf $name;paru --clean; done
