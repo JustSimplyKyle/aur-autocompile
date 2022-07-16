4c58fdb061d841867b8f789c3eea263ab320948d
KyleUltimate <68589851+KyleUltimate@users.noreply.github.com>
2022-07-16 12:52:11 +0800

    fuck space

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir build
 cd build
-mkdir pacarcarx;string=$(wc -l ../packages.aur); counts=${string%% *};for (( i=1; i<=$counts; i++ )); do echo $i;name=$(awk "NR==$i {print; exit}" ../packages.aur);echo $name;git clone "https://aur.archlinux.org/$name.git" ; cd $name; makepkg -s --noconfirm;cp $name*.pkg* ../pacarcarx ; cd ../ ; done
+mkdir pacarcarx;string=$(wc -l ../packages.aur); counts=${string%% *};for (( i=1; i<=$counts; i++ )); do echo $i;name=$(awk "NR==$i {print; exit}" ../packages.aur);echo $name;git clone "https://aur.archlinux.org/$name.git" ; cd $name; makepkg -s --noconfirm;cp $name*.pkg* ../pacarcarx ; cd ../ ; rm -rf $name; done
 
