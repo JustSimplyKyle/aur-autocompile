219174554223c58c5262ddd2d4a3cb4ece13c1b4
KyleUltimate <68589851+KyleUltimate@users.noreply.github.com>
2022-07-16 13:16:32 +0800

    work you picee of shir

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
 mkdir build
 cd build
 git clone https://aur.archlinux.org/paru.git
@@ -6,5 +7,6 @@ cd paru
 makepkg -si --noconfirm
 cd ../
 rm -rf paru
-mkdir pacarcarx;string=$(wc -l ../packages.aur); counts=${string%% *};for (( i=1; i<=$counts; i++ )); do echo $i;name=$(awk "NR==$i {print; exit}" ../packages.aur);echo $name;git clone "https://aur.archlinux.org/$name.git" ; cd $name; paru -U --noconfirm;cp $name*.pkg* ../pacarcarx ; cd ../ ; rm -rf $name;paru --clean; done
+
+mkdir pacarcarx;string=$(wc -l ../packages.aur); counts=${string%% *};for (( i=1; i<=$counts; i++ )); do echo $i;name=$(awk "NR==$i {print; exit}" ../packages.aur);echo $name;git clone "https://aur.archlinux.org/$name.git" ; cd $name; paru -U --noconfirm;cp $name*.pkg* ../pacarcarx ; cd ../ ; rm -rf $name;paru --clean --noconfirm; done
 
